67e74059e7b1c9495267432361c4a76ee98ac5be
Konstantin Melnikov <k.melnikov@velvetech.ru>
2021-10-10 15:48:01 +0300

    Updates

---
+++ static/js/main.632f8d53.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpauto-mash"]=this["webpackJsonpauto-mash"]||[]).push([[0],{10:function(t,n,e){},15:function(t,n,e){"use strict";e.r(n);var c=e(0),s=e.n(c),o=e(3),a=e.n(o),i=(e(8),e(1)),r=function(){return Object(i.jsx)("div",{className:"App",children:"App"})};e(10),e(11),e(12),e(13),e(14);a.a.render(Object(i.jsx)(s.a.StrictMode,{children:Object(i.jsx)(r,{})}),document.getElementById("root"))},8:function(t,n,e){}},[[15,1,2]]]);
+//# sourceMappingURL=main.632f8d53.chunk.js.map
