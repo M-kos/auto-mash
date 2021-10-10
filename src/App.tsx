44098344cce4989b18c50b15b24d6364e289ba60
Konstantin Melnikov <k.melnikov@velvetech.ru>
2021-10-10 18:10:46 +0300

    add logo image

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,16 @@ import { ThemeProvider } from '@mui/material'
 import { SnackbarProvider } from 'notistack'
 import DateAdapter from '@mui/lab/AdapterDateFns'
 import { theme } from './mui-theme/theme'
+import { Routing } from './routes/routing'
 
 export const App = () => {
   return (
     <div className="App">
       <ThemeProvider theme={theme}>
         <LocalizationProvider dateAdapter={DateAdapter}>
-          <SnackbarProvider maxSnack={3}>App</SnackbarProvider>
+          <SnackbarProvider maxSnack={3}>
+            <Routing />
+          </SnackbarProvider>
         </LocalizationProvider>
       </ThemeProvider>
     </div>
